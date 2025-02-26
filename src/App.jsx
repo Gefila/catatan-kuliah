6179956279279ac5b99aeed29d3a4e0311efe396
gefila <gefilazonapranata@gmail.com>
2025-02-26 20:50:26 +0700

    update to tailwind v4 and shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import Dashboard from "./pages/Dashboard";
-import LoginPages from "./pages/LoginPage";
-import MatkulPage from "./pages/PertemuanPage";
-import RegisterPage from "./pages/RegisterPage";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-    return (
-        <>
-            <MatkulPage></MatkulPage>
-        </>
-    );
+	const [count, setCount] = useState(0);
+
+	return (
+    <>
+      <h1 class="text-1xl font-bold underline">Hello world!</h1>
+      <Button variant="default" onClick={() => setCount(count + 1)}>
+        {count}
+      </Button>
+    </>
+  )
 }
 
 export default App;
